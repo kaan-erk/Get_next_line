84274efc07eedbee0bafdd455bca6b76c5df7aff
kaan-erk <kaanerktoraman@gmail.com>
2024-12-13 20:08:25 +0300

    Add files via upload

---
+++ get_next_line_utils.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktoraman <ktoraman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 17:51:08 by ktoraman          #+#    #+#             */
+/*   Updated: 2024/12/13 15:27:24 by ktoraman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*src1;
+	char			*src2;
+	char			*ptr;
+	unsigned int	i;
+	unsigned int	j;
+
+	ptr = (char *)malloc((ft_strlen(s1) + ft_strlen(s2)) * sizeof(char) + 1);
+	if (!ptr)
+		return (NULL);
+	src1 = (char *)s1;
+	src2 = (char *)s2;
+	i = 0;
+	j = 0;
+	while (src1[i])
+	{
+		ptr[i] = src1[i];
+		i++;
+	}
+	while (src2[j])
+	{
+		ptr[i + j] = src2[j];
+		j++;
+	}
+	ptr[i + j] = '\0';
+	return (ptr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+	int	len;
+
+	len = ft_strlen(s);
+	i = 0;
+	while (i <= len)
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_strdup(const char *src)
+{
+	int		len;
+	int		i;
+	char	*dest;
+
+	len = 0;
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	size_t	count;
+	size_t	size;
+	char	*tab;
+
+	if (!s)
+		return (NULL);
+	if ((unsigned int)ft_strlen(s) < start)
+		return (ft_strdup(""));
+	size = ft_strlen(s + start);
+	if (size < len)
+		len = size;
+	tab = (char *)malloc((len + 1) * sizeof(char));
+	if (!tab)
+		return (NULL);
+	count = 0;
+	while (count < len)
+	{
+		tab[count] = s[start + count];
+		count++;
+	}
+	tab[count] = '\0';
+	return (tab);
+}
